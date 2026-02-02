7e3352f3fe6e4c147357cba8a88d8d5284167652
Astrolordus <diman9286@yandex.ru>
2026-02-02 15:33:27 +0300

    Third commit

--- server.js
+++ server.js
@@ -1,38 +1,31 @@
-const http = require('http');
-const fs = require('fs'); 
-const PORT = 3000;
+import 'dotenv/config';
+import http from 'http';
+import * as fs from 'fs';
+
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-  
-    /*fs.readFile('./index.html', 'utf8', (err, data) => {
-      if (err) {
-        res.writeHead(500);
-        return res.end('Ошибка загрузки страницы');
-      }
-      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-      res.end(data);
-    });*/
-	 res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-    res.end('Сервер работает на Node.js!');
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
-    res.end('Page not found');
-  }
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end('Сервер работает на Node.js!');
+    } else {
+        res.writeHead(404);
+        res.end('Page not found!');
+    }
+    
+    
+    const logMessage = `${new Date().toString()} | ${req.method} ${req.url} | Status: ${res.statusCode}`;
+    console.log(logMessage); 
+    fs.appendFile('server.log', logMessage + '\n', (err) => {
+        if (err) console.error('Ошибка записи:', err);
+    });
 });
-
+const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+console.log(data);
 server.listen(PORT, () => {
-  console.log(` HTML-сервер запущен: http://localhost:${PORT}`);
-  function logRequest(req, status, message = '') {
-  const timestamp = new Date().toISOString();
-  const logMessage = `${timestamp} | ${req.method} ${req.url} | Status: ${status} | ${message}\n`;
-  
-  
-  console.log(logMessage.trim());
-  
-  
-  fs.appendFile('server.log', logMessage).catch(err => {
-    console.error('Ошибка записи в лог:', err.message);
-  });
-}
-}); 
+    console.log(`HTML-сервер запущен: http://${HOST}:${PORT}`);
+});
